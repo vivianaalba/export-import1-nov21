03f15af7b543fb980471a326192150bbc01bf56a
viviana <vivianaalba7@gmail.com>
2023-11-30 13:50:59 -0800

    edit code on calculator.js

--- calculator.js
+++ calculator.js
@@ -1,10 +1,23 @@
-import add from './add.js';
+// Add
+import add from '/add.js';
 
 const addResult = add(5,3);
 console.log(`Result of adddition: ${addResult}`);
 
-
-import subtract from './subtraction.js';
+// Subtract
+import subtract from '/subtraction.js';
 
 const subResult = subtract(5,3);
-console.log(`Result of adddition: ${subResult}`);
+console.log(`Result of subtraction: ${subResult}`);
+
+// Divide
+import divide from '/division.js';
+
+const divideResult = divide(6,3);
+console.log(`Result of division: ${divideResult}`);
+
+// Multiply
+import multiply from '/multiplication.js';
+
+const multResult = multiply(5,3);
+console.log(`Result of multiplication: ${multResult}`);
--- index.js
+++
@@ -1,4 +0,0 @@
-import add from './add.js';
-
-const result = add(5,3);
-console.log(`Result of adddition: ${result}`);
