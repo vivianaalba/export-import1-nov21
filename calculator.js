5078642eadcc392ee95abe5659a69426f3aedc53
viviana <vivianaalba7@gmail.com>
2023-11-30 14:03:00 -0800

    commit

--- calculator.js
+++ calculator.js
@@ -1,23 +1,23 @@
 // Add
-import add from '/add.js';
+import add from './add.js';
 
 const addResult = add(5,3);
 console.log(`Result of adddition: ${addResult}`);
 
 // Subtract
-import subtract from '/subtraction.js';
+import subtract from './subtraction.js';
 
 const subResult = subtract(5,3);
 console.log(`Result of subtraction: ${subResult}`);
 
 // Divide
-import divide from '/division.js';
+import divide from './division.js';
 
 const divideResult = divide(6,3);
 console.log(`Result of division: ${divideResult}`);
 
 // Multiply
-import multiply from '/multiplication.js';
+import multiply from './multiplication.js';
 
 const multResult = multiply(5,3);
 console.log(`Result of multiplication: ${multResult}`);
