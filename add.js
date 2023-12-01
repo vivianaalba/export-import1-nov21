b9f006d452176dda2f38210ebe092ff4f372ce00
viviana <vivianaalba7@gmail.com>
2023-11-30 17:36:36 -0800

    lodash edits

--- add.js
+++ add.js
@@ -1,5 +1,9 @@
-import _ from "lodash"
+import isInteger from "lodash"
 
 export default function add(num1, num2) {
-    return num1 + num2;
-}
+    if (_.isInteger(num1) && _.isInteger(num2)) {
+        return num1 + num2 ;
+    } else {
+        return NaN;
+    }
+};
